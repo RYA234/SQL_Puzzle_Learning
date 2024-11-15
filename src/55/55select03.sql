5e06eb1fcf0548c87e4560d340eddc7556ca136d
RYA234 <103587568+RYA234@users.noreply.github.com>
2024-11-15 21:48:05 +0000

    55 競走馬の入賞回数　その３　p230 完了

--- src/55/55select03.sql
+++ src/55/55select03.sql
@@ -1,4 +1,27 @@
+-- 55 競走馬の入賞回数　その３　p230
 SELECT H1.horse,
+        (SELECT COUNT(*) 
+           FROM RacingResults AS R1
+              WHERE R1.win_name = H1.horse)
+      + (SELECT COUNT(*) 
+           FROM RacingResults AS R1
+              WHERE R1.place_name = H1.horse)
+      + (SELECT COUNT(*) 
+           FROM RacingResults AS R1
+              WHERE R1.show_name = H1.horse)
+    FROM HOrseNames AS H1;
+
+-- 途中式　各々の項目を表示するだけ
+SELECT H1.horse,
+        (SELECT COUNT(*) 
+           FROM RacingResults AS R1
+              WHERE R1.win_name = H1.horse) as C1,
+      (SELECT COUNT(*) 
+           FROM RacingResults AS R1
+              WHERE R1.place_name = H1.horse) as C2,
+       (SELECT COUNT(*) 
+           FROM RacingResults AS R1
+              WHERE R1.show_name = H1.horse) as C3,
         (SELECT COUNT(*) 
            FROM RacingResults AS R1
               WHERE R1.win_name = H1.horse)
